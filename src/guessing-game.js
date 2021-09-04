53070ae5a01e0885bb9094a031c36420ce79e4d1
thirdmadman <thirdmadman@gmail.com>
2021-09-04 20:08:34 +0400

    feat: impl guessing-game

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,21 +1,38 @@
 class GuessingGame {
-    constructor() {}
+  constructor() {}
+  /**
+   * This method accepts min and max value of range of number to guess
+   * @param {*} min
+   * @param {*} max
+   */
 
-    setRange(min, max) {
+  setRange(min, max) {
+    this.min = min;
+    this.max = max;
+  }
 
-    }
+  /**
+   * This method returns solution candidate (you make an assumption based on range and previous assumptions)
+   * @returns
+   */
+  guess() {
+    this.middle = Math.round((this.min + this.max) / 2);
+    return this.middle;
+  }
 
-    guess() {
+  /**
+   * This method is called if prev call of guess() returned number which is greater than answer
+   */
+  lower() {
+    this.max = this.middle;
+  }
 
-    }
-
-    lower() {
-
-    }
-
-    greater() {
-
-    }
+  /**
+   * This method is called if prev call of guess() returned number which is lower than answer
+   */
+  greater() {
+    this.min = this.middle;
+  }
 }
 
 module.exports = GuessingGame;
